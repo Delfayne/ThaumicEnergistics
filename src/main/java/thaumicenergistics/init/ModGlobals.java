31b7e88d2042dab1f5650d0f22b5e3ac0c7d4226
Delfayne <delfayne@users.noreply.github.com>
2024-04-21 10:26:45 +0900

    Replacing MOD_NAME with generated

--- src/main/java/thaumicenergistics/init/ModGlobals.java
+++ src/main/java/thaumicenergistics/init/ModGlobals.java
@@ -12,7 +12,6 @@ import thaumicenergistics.api.ThEApi;
  */
 public class ModGlobals {
 
-    public static final String MOD_NAME = "Thaumic Energistics";
     public static final String MOD_VERSION = "2.2.6";
     public static final String MOD_DEPENDENCIES = "required-after:appliedenergistics2@[rv6-stable-6,);" +
             "required-after:thaumcraft@[6.1.BETA26,);" +
