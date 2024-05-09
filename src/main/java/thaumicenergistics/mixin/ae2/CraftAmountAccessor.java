4e775528408eea8882b014badd4d2249cad3e125
Zengyj <zengyj11111@gmail.com>
2024-05-09 14:20:56 +0800

    Fix crash for latest AE2-UEL

--- src/main/java/thaumicenergistics/mixin/ae2/CraftAmountAccessor.java
+++ src/main/java/thaumicenergistics/mixin/ae2/CraftAmountAccessor.java
@@ -2,6 +2,7 @@ package thaumicenergistics.mixin.ae2;
 
 import appeng.client.gui.implementations.GuiCraftAmount;
 import appeng.client.gui.widgets.GuiNumberBox;
+import net.minecraft.client.gui.GuiTextField;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import thaumicenergistics.annotation.ClientOnlyMixin;
@@ -12,5 +13,5 @@ import thaumicenergistics.annotation.LateMixin;
 @LateMixin
 public interface CraftAmountAccessor {
     @Accessor(remap = false)
-    GuiNumberBox getAmountToCraft();
+    GuiTextField getAmountToCraft();
 }
